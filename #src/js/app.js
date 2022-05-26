a9d4199b0cf473009971fe20fda2519c98fb03ca
V Zhevaha <v.zhevaga@gmail.com>
2022-05-26 18:12:36 +0200

    first commit

---
+++ #src/js/app.js
@@ -0,0 +1,8 @@
+console.log("JS start!");
+
+import * as flsFunctions from "./files/functions.js";
+flsFunctions.isWebp();
+
+import * as burger from 					"./files/burger.js";
+import * as scrolls from 					"./files/scrolls.js";
+import * as dynamicAdaptive from 	"./files/d_adaptiv.js";
