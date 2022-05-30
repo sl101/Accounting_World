3465fde7469dfe0e77ffd3f7c77d98727db1a8a2
V Zhevaha <v.zhevaga@gmail.com>
2022-05-30 22:49:23 +0200

    fixed top body

--- #src/js/files/submenu.js
+++ #src/js/files/submenu.js
@@ -10,14 +10,14 @@ let isMobile = {
 if(isMobile.any()){
 		body.classList.add('touch');
 		let arrow=document.querySelectorAll('.arrow');
-		// console.log("length = " + arrow.length);
+
 	for(let i = 0; i<arrow.length; i++){
 			let thisLink=arrow[i].previousElementSibling;
 			let subMenu=arrow[i].nextElementSibling;
 			let thisArrow=arrow[i];
 
 			thisLink.classList.add('parent');
-		arrow[i].addEventListener('click', function(){
+			arrow[i].addEventListener('click', function(){
 			subMenu.classList.toggle('open');
 			thisArrow.classList.toggle('active');
 		});
